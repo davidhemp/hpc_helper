56b2e0a6801501fbc47a4a01326bfd7c27c8c749
D.W.Hempston <cyber_veio@hotmail.com>
2020-04-06 15:19:51 +0100

    Can pull Lyceum applications usng the work in progress method

--- serviceline/Lyceum_applications.py
+++ serviceline/Lyceum_applications.py
@@ -3,15 +3,13 @@ import subprocess
 
 import pysnow
 
-#Password and user are kept seperate. 
+#Password and user are kept seperate.
 from hpc_key import *
 
-with open("/home/dwh1d17/.ssh/admdh.key") as f:
-    admdh_passwd = f.read().strip("\n")
-adcli = "/mainfs/home/dwh1d17/tools/hpc_helper/serviceline/adcli.sh"
+import manage_ldap
 
 client = pysnow.client.Client(host='sotonproduction.service-now.com',
-                              user=user, password=password)
+                              user=sl_user, password=sl_password)
 
 request = client.resource(api_path='/v1/table/sc_req_item')
 form_request = client.resource(api_path='/uno49/ritm_iridis_api', base_path='/api')
@@ -22,32 +20,24 @@ query = pysnow.QueryBuilder() \
         .field('short_description').starts_with('Lyceum Account Application') \
         .AND().field('sys_created_on').greater_than(lower_limit)
 
-#response = request.get(query=query, stream=True)
-response = request.get(query={'number':'RITM0204120'})
+response = request.get(query=query, stream=True)
+#response = request.get(query={'number':'RITM0204120'})
 
 for record in response.all():
-    ritm = record['number']
-    print("----")
-    print(ritm)
-    variables = form_request.request('GET', headers={"ritm": ritm}).one()[ritm]
-    try:
-        if len(variables["lyceum_additional"]) > 0:
-            print("Supervisor has additional comments.")
-            print(variables["lyceum_additional"])
-            continue
-    except KeyError:
-        pass
-    for i in range(1, int(variables["lyceum_number"])+1):
-        userid = variables["lyceum_name_{}".format(i)]
-        try:
-            if variables["lyceum_gpu_{}".format(i)][0] == "Y":
-                cluster = "jfAccessLyceum5"
-            else:
-                cluster = "jfAccessLyceum4"
-        except KeyError:
-            cluster = "jfAccessLyceum4"
-        user = user_request.get(query={'sys_id': userid})
-        username = user.one()["user_name"]
-        print(username, cluster)
-        subprocess.call(["echo", "{}".format(admdh_passwd), ">", "adcli", "add-member", "--stdin-password", "-D", "soton.ac.uk", "-U", "admdh", cluster, username], shell=True)
-        #subprocess.call([adcli, admdh_passwd, cluster, username], shell=True)
+    if record['assigned_to'] != '':
+        # Only check tickets assigned to dwh1d17 and "in progress" state
+        if record['assigned_to']['value'] == '68e65aaedbb6c70014367db33c961998' and record["state"] == "2":
+            ritm = record['number']
+            print("----")
+            print(ritm)
+            variables = form_request.request('GET', headers={"ritm": ritm}).one()[ritm]
+            if len(variables["lyceum_additional"]) > 0:
+                print("Supervisor has additional comments.")
+                print(variables["lyceum_additional"])
+            for i in range(1, int(variables["lyceum_number"])+1):
+                userid = variables["lyceum_name_{}".format(i)]
+                # We now add users to both clusters
+                clusters = ["jfAccessLyceum5", "jfAccessLyceum4"]
+                user = user_request.get(query={'sys_id': userid})
+                username = user.one()["user_name"]
+                print(username, clusters)
--- serviceline/run.sh
+++ serviceline/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 module load conda
 source activate serviceline
-python Iridis_applications.py
+#python Iridis_applications.py
+python Lyceum_applications.py
