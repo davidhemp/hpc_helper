1d7c656e604c44c0084d00182059db2a5c4116c5
D.W.Hempston <cyber_veio@hotmail.com>
2020-10-26 15:12:04 +0000

    Updated Lyceum email

--- serviceline/email_users.py
+++ serviceline/email_users.py
@@ -13,7 +13,7 @@ def connect():
 def send_lyceum_email(sender_email = "hpc-noreply@soton.ac.uk", receiver_email = "dwh1d17@soton.ac.uk", full_name="David Hempston"):
     with connect() as server:
         msg = lyceum_welcome_email(sender_email, receiver_email, full_name)
-        server.sendmail(sender_email, [receiver_email, "dwh1d17@soton.ac.uk"], msg.as_string())
+        server.sendmail(sender_email, receiver_email, msg.as_string())
  
 def lyceum_welcome_email(sender_email = "hpc-noreply@soton.ac.uk", receiver_email = "dwh1d17@soton.ac.uk", full_name="David Hempston"):
     msg = MIMEMultipart()
@@ -23,16 +23,32 @@ def lyceum_welcome_email(sender_email = "hpc-noreply@soton.ac.uk", receiver_emai
 
     msgText = MIMEText("""
         <p>Dear {},</p>
-        <p>You now have access to the Lyceum service which runs on the Iridis cluster hardware. You should be able to login to one of the login nodes using your university username and password.
-        Help and support can be found on our wiki pages: https://hpc.soton.ac.uk/redmine/projects/lyceum-support/wiki. If you are new to Linux we recommend the Linkedin learning course. For any questions on how to use the cluster please put a new message on the apropiate Iridis forum. </p>
-        <p>https://hpc.soton.ac.uk/redmine/projects/iridis-4-support/boards</p>
-        <p>https://hpc.soton.ac.uk/redmine/projects/iridis-5-support/boards</p>
-        <p></p>
-        <p>You are entitled to use Lyceum as long as you are a student of the University. When you leave the University, <strong>all your data on Lyceum will be removed following grace period of three months.</strong></p>
-        <p>
-        Kind regards,<br>
-        HPC team
-        </p>
+<p>You now have access to the Lyceum service which runs on the Iridis cluster hardware. To connect, ssh to one of the login nodes using your university username and password. </p>
+<p>The Iridis 4 login nodes are:<br>
+<br>
+Iridis4_a.soton.ac.uk   # The busiest<br>
+Iridis4_b.soton.ac.uk   # The quietest<br>
+Iridis4_c.soton.ac.uk   # For long downloads<br>
+<br>
+Help and support can be found on our wiki pages: https://hpc.soton.ac.uk/redmine/projects/getting-started/wiki/Iridis_Welcome<br>
+</p>
+<p>If you not on the university network (not on campus or in halls) you will need to first connect to the university VPN. If you are on Windows or mac please follow the steps below/</p>
+<p>
+1) Open https://globalprotect.soton.ac.uk in a browser and login using your University account. <br>
+2) Select the version of the agent relevant to your OS (e.g. Windows 64 bit for windows 10).<br>
+3) The installer will download. Once complete, launch the installer and follow the wizard through the installation process.<br>
+4) Once installed a globe shaped icon will appear in the Notification area (Windows).<br>
+5) Click on the globe icon and enter the portal address 'globalprotect.soton.ac.uk'. Click 'Connect'. Enter user credentials when prompted.<br>
+6) You will now be connected to the University's VPN service. To disconnect, press 'Disconnect'. The GlobalProtect icon will always appear in the Notification area, regardless of the status of the connection.</p>
+
+<p>If you are new to Linux, then a good place to start is with Linux Basics on the wiki or follow the Linkedin learning course.</p>
+
+<p>You are entitled to use Lyceum as long as you are a student of the University. 
+When you leave the University, all your data on Lyceum will be removed following 
+grace period of three months.</p>
+
+<p>Kind regards,<br>
+HPC team</p>
         """.format(full_name), 'html')
     msg.attach(msgText)
     return msg
